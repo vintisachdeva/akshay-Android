0368bc19214867dbf6772ed5861b3e2a886af31a
vintisachdeva <vintisachdeva17@gmail.com>
2017-03-15 12:15:36 +0530

    Add files via upload

---
+++ JsonData/HttpHandler.java
@@ -0,0 +1,73 @@
+package fetchingjson.bmpl.com.fetchingjson;
+
+
+import android.nfc.Tag;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class HttpHandler {
+
+    private static final String TAG = HttpHandler.class.getSimpleName();
+
+
+    public String makeServiceCall(String reqUrl){
+        String response = null;
+
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // to read the response from the URL
+
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            response = convertStreamToString(in);
+
+        }
+
+        catch (MalformedURLException e) {
+            Log.e(TAG,"MalformedURLException " + e.getMessage());
+
+            //e.printStackTrace();
+        } catch (IOException e) {
+            Log.e(TAG,"IOException " + e.getMessage());
+            //e.printStackTrace();
+        } catch (Exception e){
+            Log.e(TAG,"Exception " + e.getMessage());
+        }
+        return response;
+    }
+
+
+    private String convertStreamToString(InputStream inputStream){
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = bufferedReader.readLine())!=null){
+                stringBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ JsonData/MainActivity.java
@@ -0,0 +1,144 @@
+package fetchingjson.bmpl.com.fetchingjson;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView listView;
+    private ProgressDialog progressDialog;
+
+    private static String url = "http://api.androidhive.info/contacts/";
+
+    ArrayList<HashMap<String,String >> contactList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactList = new ArrayList<>();
+        listView = (ListView)findViewById(R.id.listView);
+
+        new GetContacts().execute();
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Please wait");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler httpHandler = new HttpHandler();
+
+            String jsonString = httpHandler.makeServiceCall(url);
+
+            Log.e(TAG,"Response from url: " +jsonString);
+
+            if(jsonString!=null){
+                try{
+                    JSONObject jsonObject = new JSONObject(jsonString);
+
+                    JSONArray contacts = jsonObject.getJSONArray("contacts");
+                    //looping through all the contacts
+
+                    for(int i=0; i<contacts.length();i++){
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+
+                        //Phone node is json object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("mobile", mobile);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+
+                } catch (final JSONException e) {
+                    //e.printStackTrace();
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                }
+            }
+            else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (progressDialog.isShowing())
+                progressDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{"name", "email",
+                    "mobile"}, new int[]{R.id.name,
+                    R.id.email, R.id.mobile});
+
+            listView.setAdapter(adapter);
+        }
+    }
+}
